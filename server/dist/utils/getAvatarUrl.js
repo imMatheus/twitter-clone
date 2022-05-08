caaa92e81a87d924652d25c8a7f75b217682de9b
imMatheus <matheusmendesbarata03@gmail.com>
2022-05-08 15:33:21 +0200

    changed .gitingore

--- server/dist/utils/getAvatarUrl.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAvatarUrl = void 0;
-const getAvatarUrl = (seed) => {
-    return `https://avatars.dicebear.com/api/identicon/${seed}.svg`;
-};
-exports.getAvatarUrl = getAvatarUrl;
-//# sourceMappingURL=getAvatarUrl.js.map
