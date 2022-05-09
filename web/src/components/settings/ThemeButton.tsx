148c5d970ecc8f140eca80902b65cfac66bb71ed
imMatheus <matheusmendesbarata03@gmail.com>
2022-05-09 22:59:27 +0200

    Fixed theme changer

--- web/src/components/settings/ThemeButton.tsx
+++ web/src/components/settings/ThemeButton.tsx
@@ -21,11 +21,10 @@ const ThemeButton: React.FC<ThemeButtonProps> = ({ theme }) => {
 
 	return (
 		<div role="radiobutton" className={buttonStyle} onClick={onClickHandle}>
-			<div className="flex h-10 w-10 items-center justify-center rounded-full bg-transparent transition-colors group-hover:bg-gray-700/50">
-				<div className="flex h-5 w-5 items-center justify-center rounded-full border border-blue-600">
-					{isSelected && <Check className="h-4 w-4" />}
-				</div>
+			<div className={styles['circle-wrapper']}>
+				<div className={styles['circle']}>{isSelected && <Check className="h-4 w-4 text-white" />}</div>
 			</div>
+
 			<p className="font-bold">{theme.charAt(0).toUpperCase() + theme.slice(1)}</p>
 		</div>
 	)
