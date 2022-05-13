f106a09f3f608aa361a1b7154f04361b4c1c0fbf
imMatheus <matheusmendesbarata03@gmail.com>
2022-05-13 04:45:49 +0200

    added spinner

---
+++ web/src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Spinner: React.FC = ({}) => {
+	return <div className="h-7 w-7 animate-spin rounded-full border-[3px] border-border border-t-accent"></div>
+}
+
+export default Spinner
--- web/src/pages/users/[handle]/index.tsx
+++ web/src/pages/users/[handle]/index.tsx
@@ -6,16 +6,19 @@ import { UserWithTweets } from '@/types/UserWithTweets'
 import TweetsContainer from '@/components/TweetsContainer'
 import UserBanner from '@/components/UserBanner'
 import UserNotFoundBanner from '@/components/UserNotFoundBanner'
+import Spinner from '@/components/Spinner'
 
 const UserPage: NextPage = () => {
 	const router = useRouter()
 	const { handle } = router.query
-	const [user] = useSWR<UserWithTweets>(`/users/${handle}`)
+	const [user, error, loading] = useSWR<UserWithTweets>(`/users/${handle}`)
 	console.log(user)
 
 	return (
 		<div className="">
-			{user ? (
+			{loading ? (
+				<Spinner />
+			) : user ? (
 				<>
 					<UserBanner user={user} />
 					{user.tweets && user.tweets.length && <TweetsContainer tweets={user.tweets} />}
