dcc22dcd233ecf8a192ade53f897df7276dfacb5
imMatheus <matheusmendesbarata03@gmail.com>
2022-06-01 20:07:17 +0200

    fixed sidebar

--- web/src/components/HeaderBox.tsx
+++ web/src/components/HeaderBox.tsx
@@ -11,7 +11,8 @@ interface HeaderBoxProps {
 const HeaderBox: React.FC<HeaderBoxProps> = ({ goBack, title, subtitle }) => {
 	return (
 		<div className="sticky top-0 z-50 h-14">
-			<div className="absolute inset-0 bg-white/80 backdrop-blur-md"></div>
+			<div className="absolute inset-0 bg-bg opacity-80"></div>
+			<div className="absolute inset-0 backdrop-blur-md"></div>
 			<div className="relative z-50 flex h-full items-center gap-6 px-4 py-2">
 				{goBack && (
 					<Link href="/" passHref>
@@ -23,7 +24,7 @@ const HeaderBox: React.FC<HeaderBoxProps> = ({ goBack, title, subtitle }) => {
 					</Link>
 				)}
 				<div>
-					<h2 className="text-xl font-bold">{title}</h2>
+					<h2 className="text-lg font-bold">{title}</h2>
 					{subtitle && <p className="text-sm text-text-grayed">{subtitle}</p>}
 				</div>
 			</div>
