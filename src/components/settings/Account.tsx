d9538c8b2ca74553cd7893f230412f82a4db9acd
imMatheus <matheusmendesbarata03@gmail.com>
2022-07-09 21:09:20 +0200

    added sign out/in button for account settigns

--- src/components/settings/Account.tsx
+++ src/components/settings/Account.tsx
@@ -1,39 +1,54 @@
 import React, { useId, useState } from 'react'
 import Header from './Header'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import { useAuth } from '@/context/AuthContext'
 import CheckBox from '@/components/checkBox'
 import { trpc } from '@/utils/trpc'
+import Button from '@/components/button'
 
 const Account: React.FC = ({}) => {
+	const { data } = useSession()
 	const privacyMutation = trpc.useMutation(['users.updatePrivacy'])
-
 	const utils = trpc.useContext()
 
 	return (
 		<div className="border-b border-b-border p-4">
 			<Header text="Account" />
-			<div className="flex items-center">
-				<div>
-					<h3 className="text-base font-bold">Private account</h3>
-					<p className="text-sm text-text-grayed">
-						When selected, your Tweets and other account information are only visible to people who follow
-						you.
-					</p>
-				</div>
-				<CheckBox
-					onClick={(checked) => {
-						privacyMutation.mutate(
-							{
-								privacy: checked ? 'PRIVATE' : 'PUBLIC'
-							},
-							{
-								onSuccess() {
-									utils.invalidateQueries(['me'])
+			{data && (
+				<div className="flex items-center">
+					<div>
+						<h3 className="text-base font-bold">Private account</h3>
+						<p className="text-sm text-text-grayed">
+							When selected, your Tweets and other account information are only visible to people who
+							follow you.
+						</p>
+					</div>
+					<CheckBox
+						onClick={(checked) => {
+							privacyMutation.mutate(
+								{
+									privacy: checked ? 'PRIVATE' : 'PUBLIC'
+								},
+								{
+									onSuccess() {
+										utils.invalidateQueries(['me'])
+									}
 								}
-							}
-						)
-					}}
-				/>
+							)
+						}}
+					/>
+				</div>
+			)}
+			<div className="mt-4">
+				{data ? (
+					<Button onClick={() => signOut()} variant="dark" size="large">
+						Sign out
+					</Button>
+				) : (
+					<Button onClick={() => signIn()} variant="dark" size="large">
+						Sign in
+					</Button>
+				)}
 			</div>
 		</div>
 	)
--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -1,24 +1,13 @@
 import React from 'react'
 import ColorPicker from './ColorPicker'
 import ThemeButtonWrapper from './ThemeButtonWrapper'
-import { signIn, signOut, useSession } from 'next-auth/react'
 import Header from './Header'
 import Account from './Account'
 import Accent from './Accent'
 
 const Settings: React.FC = () => {
-	const { data } = useSession()
 	return (
 		<div>
-			{/* {data ? (
-					<button onClick={() => signOut()} className="bg-red-500 p-2">
-						Sign out
-					</button>
-				) : (
-					<button onClick={() => signIn()} className="bg-blue-500 p-2">
-						Sign in
-					</button>
-				)} */}
 			<Accent />
 			<ThemeButtonWrapper />
 			<Account />
