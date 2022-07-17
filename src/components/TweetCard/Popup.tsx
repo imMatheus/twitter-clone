33e3c406f6f30451561d9944f093e289c908e82c
imMatheus <matheusmendesbarata03@gmail.com>
2022-07-17 19:51:29 +0200

    did a thing

--- src/components/TweetCard/Popup.tsx
+++ src/components/TweetCard/Popup.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import PopupRow from './PopupRow'
 import { useClickOutside } from '@mantine/hooks'
-import { Frown, UserPlus, UserX, List, Trash2 } from 'react-feather'
+import { UserPlus, User, Trash2, ExternalLink } from 'react-feather'
 import { Tweet as ITweet } from '@/types/Tweet'
 import { useSession } from 'next-auth/react'
 import { trpc } from '@/utils/trpc'
+import { useRouter } from 'next/router'
 
 interface PopupProps {
 	toggle: React.Dispatch<React.SetStateAction<boolean>>
@@ -15,6 +16,10 @@ const Popup: React.FC<PopupProps> = ({ toggle, tweet }) => {
 	const ref = useClickOutside(() => toggle(false))
 	const deleteTweetMutation = trpc.useMutation('tweets.deleteById')
 	const { data: session } = useSession()
+	const { data: follows } = trpc.useQuery(['users.currentUserFollowsUser', { id: tweet.owner.id }])
+	const currentlyFollows = follows?.follows
+	const followMutation = trpc.useMutation('users.follow')
+	const router = useRouter()
 
 	const deleteHandler = () => {
 		deleteTweetMutation.mutate({ id: tweet.id })
@@ -31,7 +36,7 @@ const Popup: React.FC<PopupProps> = ({ toggle, tweet }) => {
 					box-shadow: rgb(var(--text) / 20%) 0px 0px 15px, rgb(var(--text) / 15%) 0px 0px 3px 1px;
 				}
 			`}</style>
-			{session && session.userId === tweet.owner.id && (
+			{session && session.userId === tweet.owner.id ? (
 				<li
 					className="flex gap-3 p-4 text-danger transition-colors hover:bg-text/[0.03]"
 					onClick={deleteHandler}
@@ -39,12 +44,53 @@ const Popup: React.FC<PopupProps> = ({ toggle, tweet }) => {
 					<Trash2 className="h-5 w-5" />
 					<p className="font-normal">Delete Tweet</p>
 				</li>
+			) : (
+				<>
+					<PopupRow
+						Icon={UserPlus}
+						onClick={() => {
+							followMutation.mutate(
+								{ id: tweet.owner.id },
+								{
+									onSuccess() {}
+								}
+							)
+						}}
+					>
+						{currentlyFollows ? 'Unfollow' : 'Follow'} @{tweet.owner.handle}
+					</PopupRow>
+
+					<PopupRow
+						Icon={UserPlus}
+						onClick={() => {
+							followMutation.mutate(
+								{ id: tweet.owner.id },
+								{
+									onSuccess() {}
+								}
+							)
+						}}
+					>
+						{currentlyFollows ? 'Unfollow' : 'Follow'} @{tweet.owner.handle}
+					</PopupRow>
+				</>
 			)}
-			<PopupRow Icon={Frown}>Not interested in this Tweet</PopupRow>
-			<PopupRow Icon={UserPlus}>Follow@kylegawley</PopupRow>
-			<PopupRow Icon={List}>Add/remove @kylegawley from Lists</PopupRow>
-			<PopupRow Icon={UserPlus}>Mute @kylegawley</PopupRow>
-			<PopupRow Icon={UserPlus}>Block @kylegawley</PopupRow>
+			<PopupRow
+				Icon={User}
+				onClick={() => {
+					router.push(`/users/${tweet.owner.handle}`)
+				}}
+			>
+				Visit {tweet.owner.name}
+			</PopupRow>
+			<PopupRow
+				Icon={ExternalLink}
+				onClick={() => {
+					router.push(`/users/${tweet.owner.handle}/tweets/${tweet.id}`)
+				}}
+			>
+				Visit Tweet
+			</PopupRow>
 		</ul>
 	)
 }
--- src/components/TweetCard/PopupRow.tsx
+++ src/components/TweetCard/PopupRow.tsx
@@ -4,11 +4,12 @@ import type { Icon as IconType } from 'react-feather'
 interface PopupRowProps {
 	Icon: IconType
 	children: React.ReactNode
+	onClick: () => void
 }
 
-const PopupRow: React.FC<PopupRowProps> = ({ Icon, children }) => {
+const PopupRow: React.FC<PopupRowProps> = ({ Icon, children, onClick }) => {
 	return (
-		<li className="flex gap-3 p-4 transition-colors hover:bg-text/[0.03]">
+		<li className="flex gap-3 p-4 transition-colors hover:bg-text/[0.03]" onClick={onClick}>
 			<Icon className="h-5 w-5" />
 			<p className="font-normal text-text">{children}</p>
 		</li>
